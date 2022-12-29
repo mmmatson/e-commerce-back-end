d43e677cc44cf5bbd016d44115984d35b62c4c8a
mmm <margaret.m.matson@gmail.com>
2022-12-29 13:17:45 -0600

    fixed bug - alias name same as another model's name

--- models/index.js
+++ models/index.js
@@ -21,7 +21,7 @@ Product.belongsToMany(Tag, {
     model: ProductTag,
     unique: false
   },
-  as: 'product_tag'
+  as: 'product_tags'
 });
 
 // Tags belongToMany Products (through ProductTag)
@@ -30,7 +30,7 @@ Tag.belongsToMany(Product, {
     model: ProductTag,
     unique: false
   },
-  as: 'tag_product'
+  as: 'tag_products'
 });
 
 module.exports = {
